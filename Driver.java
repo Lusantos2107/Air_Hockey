21fef40c4769bc1af030b23e08f85d893ae93526
Lusantos2107 <l.monteirosantos@lancaster.ac.uk>
2023-05-18 22:06:44 +0100

    Movement for the mallets are done

--- Driver.java
+++ Driver.java
@@ -4,12 +4,79 @@ public class Driver
   {
     GameArena Arena = new GameArena (1200, 700);
     Table table = new Table(100, 100);
-    Mallet mallet = new Mallet(100, 100);
+    Mallet player1 = new Mallet(100, 100);
+    Mallet player2 = new Mallet(600, 100);
+    Puck puck = new Puck(100, 100);
 
-
+    //Content to add to the GameArena 
     table.addtable(Arena);
-    mallet.addmallet(Arena);
-  
+    player1.addmallet(Arena);  
+    player2.addmallet(Arena);
+    puck.addpuck(Arena);
+
+
+
+    //While loop constantly running to see if key has been pressed
+    while (true){
+
+      if (Arena.upPressed()){
+        player2.move(0, -10);
+      }
+      if (Arena.downPressed()){
+        player2.move(0, 10);
+      }
+      if (Arena.leftPressed()){
+        player2.move(-10, 0);
+      }
+      if (Arena.rightPressed()){
+        player2.move(10, 0);
+      }
+      if (Arena.letterPressed('w')){
+        player1.move(0, -10);
+      }
+      if (Arena.letterPressed('s')){
+        player1.move(0, 10);
+      }
+      if (Arena.letterPressed('a')){
+        player1.move(-10, 0);
+      }
+      if (Arena.letterPressed('d')){
+        player1.move(10, 0);
+      }
+
+
+  //Stops the mallets and puck from escaping the table, 
+  //Limiting player one for the left secion and player 2 to the right 
+
+      if (player1.getYPosition()<= 130){
+        player1.move(0, 10);
+      }
+      if (player2.getYPosition()<= 130){
+        player2.move(0, 10);
+      }
+      if (player1.getYPosition()>= 570){
+        player1.move(0, -10);
+      }
+      if (player2.getYPosition()>= 570){
+        player2.move(0, -10);
+      }
+
+      if (player1.getXPosition()<= 130){
+        player1.move(10, 0);
+      }
+      if (player2.getXPosition()<= 1060){
+        player2.move(10, 0);
+      }
+      if (player1.getXPosition()>= 570){
+        player1.move(-10, 0);
+      }
+      if (player2.getXPosition()>= 650){
+        player2.move(-10, 0);
+      }
+      //acts a buffer for 0.15 milliseconds to record the movements
+      Arena.pause();
+
+    }
 
 
 
--- Mallet.java
+++ Mallet.java
@@ -1,67 +1,39 @@
 public class Mallet {
 
 
-    private Ball mallet1 = new Ball(0, 0, 80, "RED", 1);
-    private Ball mallet2 = new Ball(0, 0, 80, "RED", 1);
+    private Ball mallet = new Ball(0, 0, 80, "RED", 2);
+  
 
-
+	//sets the position of the mallets 
     public Mallet(double x, double y)
 	{
-        mallet1.setXPosition(x + 250);
-        mallet1.setYPosition(y + 250);
+        mallet.setXPosition(x + 250);
+        mallet.setYPosition(y + 250);
 
-        mallet2.setXPosition(x + 750);
-        mallet2.setYPosition(y + 250);
 
     }
+	//Allows for the movement of the mallets 
     public void move(double dx, double dy)
 	{
-		mallet1.setXPosition(50);
-		mallet1.setYPosition(50);
-	}
-
-    //public void keyPressed(KeyEvent e)
-	//{
-	//	keyAction(e,true);
-	//}
-
-	//public void keyAction(KeyEvent e,boolean yn)
-	//{
-	//	int code = e.getKeyCode();
-
-	//	if (code == KeyEvent.VK_UP)
-	//		up = yn;
-	//	if (code == KeyEvent.VK_DOWN)
-	//		down = yn;
-	//	if (code == KeyEvent.VK_LEFT)
-	//		left = yn;
-	//	if (code == KeyEvent.VK_RIGHT)
-	//		right = yn;
-	//	if (code == KeyEvent.VK_SPACE)
-	//		space = yn;
-	//	if (code == KeyEvent.VK_SHIFT)
-	//		shift = yn;
-	//	if (code == KeyEvent.VK_ESCAPE)
-	//		esc = yn;
-	//	if (code == KeyEvent.VK_ENTER)
-	//		enter = yn;
-	//	//ASCII A-Z check
-	//	if (isAsciiAZ(code))
-	//		alphasPressed[code-asciiAZOffset] = yn;
-	//}
-
-	//public void keyReleased(KeyEvent e){
-	//	keyAction(e,false);
-	//}
-    
-
-    
-
+		mallet.move(dx, dy);
+    }
+	//Adds mallet (ball) to the GameArena
     public void addmallet(GameArena Arena)
     {
-        Arena.addBall(mallet1);
-        Arena.addBall(mallet2);
+        Arena.addBall(mallet);
 
     }
+    //Returning the X and Y position of the mallets
+    public double getYPosition(){
+        return mallet.getYPosition();
+    }
+    public double getXPosition(){
+        return mallet.getXPosition();
+    }
+
+
+
+
 
 }
+
